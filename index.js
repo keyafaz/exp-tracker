19204d2922e0f4f0516559d1d8961b7e4f886400
jtabach <jeff.n.tabachnick@gmail.com>
2018-02-18 10:59:18 -0800

    express render and webpack config

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
-const app = express();
+const path = require('path');
+const React = require('react');
+const renderToString = require('react-dom/server').renderToString;
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello World'));
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use('/', (req, res) => {
+  res.render('index');
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/client.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            'react',
+            'stage-0',
+            ['env', { targets: { browsers: ['last 2 versions'] } } ]
+          ]
+        }
+      }
+    ]
+  }
+};
